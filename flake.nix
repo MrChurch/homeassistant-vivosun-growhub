6d23f92fea4da01b507affb2c7e8a884e7660de1
lientry <266011047+lientry@users.noreply.github.com>
2026-08-22 22:21:03 +0200

    chore(nix): add repository environment baseline

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "Vivosun GrowHub preliminary repository environment";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = [ pkgs.git pkgs.gh ];
+      };
+    };
+}
